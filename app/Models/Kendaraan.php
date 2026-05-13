adece7b75f0be8af740f1dfc4df3ad9e012e1fbe
triposasharon-hash <email@triposasharon@gmail.com>
2026-05-13 15:08:10 +0700

    projek uts e-bengkel

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan'
+    ];
+
+    public $timestamps = true;
 }
